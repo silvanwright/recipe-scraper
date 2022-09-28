7701af0dbf0b16dedf99b241c9cd1f94a70c0591
Silvan Wright <silvancaerwyn@gmail.com>
2022-09-28 11:50:22 -0400

    clean up urls

--- bscrap.py
+++ bscrap.py
@@ -1,22 +1,21 @@
 from bs4 import BeautifulSoup
 import requests
 import lxml
-import re
 
-urlstart = "href=\""
-urlend = "\" title"
+urlstart = 'href="'
+urlend = '" title'
+base_url = "https://recipes.fandom.com/"
 
 def get_url(str):
-    idx1 = str.index(urlstart)
-    idx2 = str.index(urlend)
-    url = str[idx1+len(urlstart): idx2]
-    print(url)
+    index1 = str.index(urlstart)
+    index2 = str.index(urlend)
+    url = str[index1+len(urlstart): index2]
+    return base_url+url
 
-print(urlstart)
-print(urlend)
-
-html_text = requests.get('https://recipes.fandom.com/wiki/Category:Main_Dish_Recipes').text
+html_text = requests.get('https://recipes.fandom.com/wiki/Category:Main_Dish_Recipes').text 
 soup = BeautifulSoup(html_text, 'lxml')
-recipes = soup.find_all('li', class_='category-page__member')
+recipes = soup.find_all('li', class_='category-page__member') #TODO: exclude category links
 
-get_url('member-link" href="/wiki/Outdoor_Beef_and_Rice_Skillet" title="Outdoor Beef and Rice Skillet">Outdoor Beef and Rice Skillet</a> </li>')
+for recipe in recipes:
+    url = get_url(str(recipe))
+    print(url)
