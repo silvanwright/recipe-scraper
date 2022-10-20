f0f0ac15d03be399ac96dc149d4947300efb2f28
Silvan Wright <silvancaerwyn@gmail.com>
2022-10-20 14:46:38 -0400

    Clean up get_ingredients function, deal with strangely listed directions, make crawler only crawl

--- bscrap.py
+++ bscrap.py
@@ -7,40 +7,41 @@ import sqlite3
 from sqlalchemy import null
 
 base_url = "https://recipes.fandom.com"
+ingredients_headings=("Ingredients")
+directions_headings=("Directions", "Instructions", "Method", "Steps", "Preparation")
 
 def get_ingredients(url):
     recipe_text=requests.get(url).text
     soup = BeautifulSoup(recipe_text, 'lxml')
     ingredients=[]
-    try:
-        ingredients_heading = soup.find('span', {"id": lambda x: x and x.startswith('Ingredients')}).find_parent()
-    except AttributeError:
-        return null
-    next_sib = ingredients_heading.nextSibling
+    ingredients_heading=None
+    for heading in ingredients_headings:
+        try:
+            ingredients_heading = soup.find('span', {"id": lambda x: x and x.startswith('Ingredients')}).find_parent()
+        except AttributeError:
+            continue
+    if not ingredients_heading:
+        ingredients="Ingredients not found"
+        return ingredients
+    next_sib = ingredients_heading
     while True:
         next_sib = next_sib.nextSibling
-        try:
-            tag_name = next_sib.name
-        except AttributeError:
+        if not next_sib:
             break
-        if (tag_name == "ul"):
+        tag_name = next_sib.name
+        if (tag_name in ("ol", 'ul')):
             for li in next_sib.find_all('li'):
                 ingredients.append(li.text)
         elif (tag_name == "h3"):
-            clean_h3 = next_sib.text[:-2]
-            ingredients.append(clean_h3)
+            subheading = next_sib.find('span', class_='mw-headline')
+            ingredients.append(subheading.text)
         elif (tag_name == 'p'):
-            clean_paragraph = next_sib.text.replace('\n', '')
-            if clean_paragraph!='':
-                ingredients.append(clean_paragraph)
-        elif (tag_name=='div'):
+            ingredients_list=next_sib.text.split("\n")
+            for ingredient in ingredients_list:
+                if (ingredient!=""):
+                    ingredients.append(ingredient)
+        elif (tag_name in ('div', 'figure', None)):
             continue
-        elif (tag_name=='figure'):
-            continue
-        elif (tag_name==None):
-            continue
-        elif (tag_name == "h2"):
-            break
         else:
             break
     return ingredients            
@@ -50,13 +51,13 @@ def get_directions(url):
     soup = BeautifulSoup(recipe_text, 'lxml')
     directions = []
     directions_heading=None
-    for heading in ("Directions", "Instructions", "Method"):
+    for heading in (directions_headings):
         try:
             directions_heading = soup.find('span', {"id": lambda x: x and x.startswith(heading)}).find_parent()        
         except AttributeError:
             continue
     if not directions_heading:
-        directions = "Not found"
+        directions = "Directions not found"
         return directions
     next_sib = directions_heading
     while True:
@@ -64,20 +65,20 @@ def get_directions(url):
         if not next_sib:
             break
         tag_name=next_sib.name
-        if (tag_name == "ol" or tag_name == 'ul'):
+        if (tag_name in ("ol", 'ul')):
             for li in next_sib.find_all('li'):
                 directions.append(li.text)
         elif (tag_name == "p"):
             next_text = " ".join((next_sib.text).split())  #get rid of empty paragraphs
             splitter=(next_text).replace(". ", ".OCELOT-SPLEENS")
-            directions_list=splitter.split("OCELOT-SPLEENS")  #Split paragraph by sentence without removing periods.
+            directions_list=splitter.split("OCELOT-SPLEENS")  #Split paragraph by sentence
             for step in directions_list:
-                if (step!=""):  #get rid of empty lines
+                if (step!=""):
                     directions.append(step)
         elif (tag_name == "h3"):
-            clean_h3 = next_sib.text[:-2]
-            if (clean_h3!="Other Links"):
-                directions.append(clean_h3)
+            subheading = next_sib.find('span', class_='mw-headline')
+            if (subheading.text!="Other Links"):
+                directions.append(subheading.text)
         elif (tag_name==None):
             continue
         else:
@@ -93,7 +94,7 @@ def get_categories(url):
         categories.append(category.text)
     return categories                                 
 
-def parse_recipes(recipes):
+def parse_recipes(recipes): #TODO: parse one recipe at a time, and include serving, image and copyright info where present
     count = 0
     recipe_list=[]
     for recipe in recipes:
@@ -120,32 +121,37 @@ def parse_recipes(recipes):
     print(f"{count} recipes parsed")
     return(recipe_list)
 
-def save_to_db(recipe_list):
-    df = pandas.DataFrame (recipe_list, columns = ['Title', 'Url', 'Ingredients', 'Directions', 'Categories'])
-    print(df)
-
+def save_to_db(df):
+    print(f"Saving to database ..... dataframe: {df}")
     conn = sqlite3.connect("recipes2.db")
     cursor=conn.cursor()
     create_sql = "CREATE TABLE IF NOT EXISTS recipes (title TEXT, url TEXT, ingredients TEXT, directions TEXT, categories TEXT)"
     cursor.execute(create_sql)
-
     df.to_sql(name="recipes", con=conn, if_exists='append', index=False)
     conn.commit
     conn.close
 
-def crawl_category(url):
+def crawl_category(url, recipe_link_list):
     html_text = requests.get(url).text 
     soup = BeautifulSoup(html_text, 'lxml')
-    recipes = soup.find_all('li', class_='category-page__member')
-    recipe_list=parse_recipes(recipes)
-    save_to_db(recipe_list)
-    print(f"saved {url} recipes to database")
+    recipe_link_items=soup.find_all('li', class_='category-page__member')
+    for link_item in recipe_link_items:
+        recipe_link_list.append(link_item)
     next_page=None
     next_page= soup.find('a', class_='category-page__pagination-next wds-button wds-is-secondary')        
-    if not next_page:
-        return recipe_list
-    else:
-        crawl_category(next_page.get('href'))
+    if next_page:
+        crawl_category(next_page.get('href'), recipe_link_list)
 
-#TODO: Next category: Dinner recipes in 'by course'
-crawl_category('https://recipes.fandom.com/wiki/Category:Brunch_Recipes')
+recipe_links=[]
+crawl_category('https://recipes.fandom.com/wiki/Category:Dinner_Recipes', recipe_links)
+recipe_data=parse_recipes(recipe_links)
+recipe_df = pandas.DataFrame (recipe_data, columns = ['Title', 'Url', 'Ingredients', 'Directions', 'Categories'])
+
+#print(recipe_df)
+#save_to_db(recipe_df)
+#TODO: save to csv
+
+######## TESTING
+# ingredients = get_ingredients('https://recipes.fandom.com/wiki/Belgian_Endive,_Spinach,_and_Radicchio_Salad_with_Mustard_Vinaigrette')
+# for ingredient in ingredients:
+#     print(ingredient)
