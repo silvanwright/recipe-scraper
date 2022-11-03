678a2fe8bae2234313e3f4db7ab913b74a31c3b9
Silvan Wright <silvancaerwyn@gmail.com>
2022-11-03 15:05:22 -0400

    Parse one recipe at a time, save to csv

--- bscrap.py
+++ bscrap.py
@@ -9,19 +9,25 @@ from sqlalchemy import null
 base_url = "https://recipes.fandom.com"
 ingredients_headings=("Ingredients")
 directions_headings=("Directions", "Instructions", "Method", "Steps", "Preparation")
+categories=('Breakfast_Recipes', 'Brunch_Recipes', 'Dessert_Recipes', 'Dinner_Recipes', 
+            'Lunch_Recipes', 'Main_Dish_Recipes', 'Salad_Recipes', 'Side_Dish_Recipes',
+            'Soup_Recipes', 'Stew_Recipes', 'Vegetarian_Recipes', 'Smoothie_Recipes',
+            'Condiment_Recipes')
+category_base_url = 'https://recipes.fandom.com/wiki/Category:'
+#TODO: crawl all categories
+#To crawl one category, insert the link below:
+category_link='https://recipes.fandom.com/wiki/Category:Dessert_Recipes'
 
-def get_ingredients(url):
-    recipe_text=requests.get(url).text
-    soup = BeautifulSoup(recipe_text, 'lxml')
+
+def get_ingredients(soup):
     ingredients=[]
     ingredients_heading=None
     for heading in ingredients_headings:
         try:
-            ingredients_heading = soup.find('span', {"id": lambda x: x and x.startswith('Ingredients')}).find_parent()
+            ingredients_heading = soup.find('span', {"id": lambda x: x and x.startswith(heading)}).find_parent()
         except AttributeError:
             continue
     if not ingredients_heading:
-        ingredients="Ingredients not found"
         return ingredients
     next_sib = ingredients_heading
     while True:
@@ -46,9 +52,7 @@ def get_ingredients(url):
             break
     return ingredients            
 
-def get_directions(url):
-    recipe_text=requests.get(url).text
-    soup = BeautifulSoup(recipe_text, 'lxml')
+def get_directions(soup):
     directions = []
     directions_heading=None
     for heading in (directions_headings):
@@ -57,7 +61,6 @@ def get_directions(url):
         except AttributeError:
             continue
     if not directions_heading:
-        directions = "Directions not found"
         return directions
     next_sib = directions_heading
     while True:
@@ -85,45 +88,35 @@ def get_directions(url):
             break
     return directions
 
-def get_categories(url):
-    recipe_text=requests.get(url).text
-    soup = BeautifulSoup(recipe_text, 'lxml')
+def get_categories(soup):
     categories = []
     category_section= soup.find('div', class_='page-header__categories').find_all('a', {"title": lambda x: x and x.startswith('Category:')})
     for category in category_section:
         categories.append(category.text)
-    return categories                                 
+    return categories
 
-def parse_recipes(recipes): #TODO: parse one recipe at a time, and include serving, image and copyright info where present
+def get_title(soup):
+    title = soup.find('h1', class_='page-header__title').text.strip()
+    title = title.replace('Oriental', 'Asian') #modernize the titles
+    return title                              
+
+def parse_recipes(recipe_links): #TODO: include serving, image and copyright info where present
     count = 0
     recipe_list=[]
-    for recipe in recipes:
-        link= recipe.find('a')
-        title = link.get('title')
-        if (title.startswith("Category:")):
-            continue
-        title = title.replace('Oriental', 'Asian') #modernize the titles        
-        url = base_url+link.get('href')
-        try:
-            ingredients = get_ingredients(url)
-        except:
-            continue
-        try:
-            directions = get_directions(url)
-        except:
-            continue
-        try:
-            categories = get_categories(url)
-        except:
-            continue
-        recipe_list.append([title, url, repr(ingredients), repr(directions), repr(categories)])
+    for link in recipe_links:
+        recipe_text=requests.get(link).text
+        soup = BeautifulSoup(recipe_text, 'lxml')
+        categories = get_categories(soup) #TODO: check for already-crawled categories and skip
+        title = get_title(soup)     
+        ingredients = get_ingredients(soup)
+        directions = get_directions(soup)
+        recipe_list.append([title, link, repr(ingredients), repr(directions), repr(categories)])
         count+=1
     print(f"{count} recipes parsed")
     return(recipe_list)
 
 def save_to_db(df):
-    print(f"Saving to database ..... dataframe: {df}")
-    conn = sqlite3.connect("recipes2.db")
+    conn = sqlite3.connect("recipes.db")
     cursor=conn.cursor()
     create_sql = "CREATE TABLE IF NOT EXISTS recipes (title TEXT, url TEXT, ingredients TEXT, directions TEXT, categories TEXT)"
     cursor.execute(create_sql)
@@ -131,27 +124,34 @@ def save_to_db(df):
     conn.commit
     conn.close
 
-def crawl_category(url, recipe_link_list):
-    html_text = requests.get(url).text 
+#traverse all pages in category starting with category_url and add all recipe urls in them to recipe_links list
+def crawl_category(category_url, recipe_link_list): 
+    html_text = requests.get(category_url).text 
     soup = BeautifulSoup(html_text, 'lxml')
-    recipe_link_items=soup.find_all('li', class_='category-page__member')
-    for link_item in recipe_link_items:
-        recipe_link_list.append(link_item)
+    category_page_links=soup.find_all('li', class_='category-page__member')
+    for link_item in category_page_links:
+        link= link_item.find('a')
+        title = link.get('title')
+        if (title.startswith("Category:")):
+               continue
+        recipe_link_list.append(base_url+link.get('href'))
     next_page=None
     next_page= soup.find('a', class_='category-page__pagination-next wds-button wds-is-secondary')        
     if next_page:
-        crawl_category(next_page.get('href'), recipe_link_list)
+        crawl_category(next_page.get('href'), recipe_links)
 
 recipe_links=[]
-crawl_category('https://recipes.fandom.com/wiki/Category:Dinner_Recipes', recipe_links)
+crawl_category(category_link, recipe_links)
 recipe_data=parse_recipes(recipe_links)
 recipe_df = pandas.DataFrame (recipe_data, columns = ['Title', 'Url', 'Ingredients', 'Directions', 'Categories'])
+#TODO: remove duplicates
 
+############ OUTPUT #############
 #print(recipe_df)
-#save_to_db(recipe_df)
-#TODO: save to csv
+save_to_db(recipe_df)
+recipe_df.to_csv('recipes.csv', mode='a', index=False)
 
-######## TESTING
+############ TESTING ##############
 # ingredients = get_ingredients('https://recipes.fandom.com/wiki/Belgian_Endive,_Spinach,_and_Radicchio_Salad_with_Mustard_Vinaigrette')
 # for ingredient in ingredients:
 #     print(ingredient)
